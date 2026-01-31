906a163a462478840dcd5cd3bd7376aebc948992
CAFÉ <pciskolargx@gmail.com>
2026-01-31 08:24:16 -0300

    whyyyy

---
+++ tw-blog/src/app/blog/5-dicas-github/page.tsx
@@ -0,0 +1,3 @@
+export default function MeuPost(){
+    return <h1>5 Dicas para dominar o GitHub</h1>
+}
---
+++ tw-blog/src/pages/blog/5-dicas-github.tsx
@@ -0,0 +1,4 @@
+export default function MeuPost(){
+    console.log("client component")
+    return <h1>5 dicas para o GitHub</h1>
+}
